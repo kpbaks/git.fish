866f81338850ea9551fd19f3db34a196a786a227
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-03-31 19:10:42 +0200

    feat: improve output of info msgs in pre-commit hook

--- conf.d/_pre_commit.fish
+++ conf.d/_pre_commit.fish
@@ -11,10 +11,13 @@ function _git_fish_check_for_pre_commit --on-variable PWD
     test -d .git; or return
     contains -- $PWD $_git_fish_git_directories_visisted; and return
 
+    # used to highlight the .pre-commit-config.yaml file when it is printed
+    set -l dot_pre_commit_config_yaml (set_color --bold)".pre-commit-config.yaml"(set_color normal)
+
     if test -f .pre-commit-config.yaml
         # check if hooks are installed
         if not test -f ./git/hooks/pre-commit
-            _git_fish_echo "a .pre-commit-config.yaml file was found."
+            _git_fish_echo "a $dot_pre_commit_config_yaml file was found."
             if command --query bat
                 command bat --plain .pre-commit-config.yaml
             else
@@ -28,8 +31,8 @@ function _git_fish_check_for_pre_commit --on-variable PWD
             _git_fish_echo "run `pre-commit install` to install them."
         end
     else
-        _git_fish_echo "no .pre-commit-config.yaml file found. skipping..."
-        _git_fish_echo "a sample .pre-commit-config.yaml file can be generated with: `pre-commit sample-config`"
+        _git_fish_echo "no $dot_pre_commit_config_yaml file found. skipping..."
+        _git_fish_echo "a sample $dot_pre_commit_config_yaml file can be generated with:" (echo "pre-commit sample-config" | fish_indent --ansi)
     end
 
     # check if we've already visited this directory

589fd704ce8f5d26683f3df8d996bdc4d3e5d281
Kristoffer Plagborg Bak Sørensen <kristoffer.pbs@tuta.io>
2023-04-05 18:08:21 +0200

    fix: improve formatting of text

--- conf.d/_pre_commit.fish
+++ conf.d/_pre_commit.fish
@@ -14,7 +14,7 @@ function _git_fish_check_for_pre_commit --on-event in_git_repo_root_directory
     set -l dot_pre_commit_config_yaml (set_color --bold)".pre-commit-config.yaml"(set_color normal)
 
     if test -f .pre-commit-config.yaml
-        _git_fish_echo "a $dot_pre_commit_config_yaml file was found."
+        _git_fish_echo "a $dot_pre_commit_config_yaml file was found in $(set_color --bold)$PWD$(set_color normal)"
         if command --query bat
             command bat --plain .pre-commit-config.yaml
         else
@@ -27,22 +27,22 @@ function _git_fish_check_for_pre_commit --on-event in_git_repo_root_directory
             _git_fish_echo "pre-commit hooks installed."
         else
             _git_fish_echo "pre-commit hooks installed."
-            _git_fish_echo "run $(echo "pre-commit autoupdate" | fish_indent --ansi) to autoupdate them."
+            _git_fish_echo "to autoupdate them, run: $(echo -n "pre-commit autoupdate" | fish_indent --ansi)"
         end
     else
         _git_fish_echo "no $dot_pre_commit_config_yaml file found. skipping..."
         set -l generate_sample_config_command "pre-commit sample-config | tee .pre-commit-config.yaml && pre-commit install"
         _git_fish_echo "a sample $dot_pre_commit_config_yaml file can be generated and installed with:"
-        echo -n "\t"
+        echo -en "\t"
         echo "$generate_sample_config_command" | fish_indent --ansi
         set -l abbreviation pcg
         if not abbr --query $abbreviation
-            _git_fish_echo "adding abbreviation: `$abbreviation` for"
-            echo -n "\t"
+            _git_fish_echo "adding abbreviation: $(set_color --reverse)$abbreviation$(set_color normal) for"
+            echo -en "\t"
             echo "$generate_sample_config_command" | fish_indent --ansi
             abbr --add $abbreviation "$generate_sample_config_command"
         end
-        _git_fish_echo "the abbreviation `$abbreviation` can be used to generate a sample $dot_pre_commit_config_yaml file."
+        _git_fish_echo "the abbreviation $(set_color --reverse)$abbreviation$(set_color normal) can be used to generate a sample $dot_pre_commit_config_yaml file."
     end
 
     # check if we've already visited this directory

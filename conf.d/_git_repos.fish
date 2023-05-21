2fcb28375858d344387b201fa2443dfe5e374c3c
kpbaks <kristoffer.pbs@tuta.io>
2023-05-21 22:31:27 +0200

    feat: use sqlite3 to store which repos has been visited

--- conf.d/_git_repos.fish
+++ conf.d/_git_repos.fish
@@ -16,28 +16,132 @@ for cmd in fzf sqlite3
     end
 end
 
-# This variable is used to persist the list of visited repos across shell sessions
-set --query GIT_REPOS_VISITED; or set --universal GIT_REPOS_VISITED
+# Create the database if it doesn't exist
+# This database will be used to store the last time a repo was visited
+set --query GIT_FISH_REPOS_DB; or set --universal GIT_FISH_REPOS_DB $__fish_user_data_dir/git.fish/repos.sqlite3
+
+mkdir -p (dirname $GIT_FISH_REPOS_DB)
+if not test -f $GIT_FISH_REPOS_DB
+    set -l schema "
+		CREATE TABLE repos (
+			path TEXT PRIMARY KEY,
+			timestamp INTEGER,
+			number_of_times_visited INTEGER DEFAULT 1
+		);
+	"
+    sqlite3 $GIT_FISH_REPOS_DB $schema
+end
+
 
 
 # Every time a directory with a .git directory is entered, store it in a universal variable
 function _git_repos_visited --on-event in_git_repo_root_directory
     set -l git_color (set_color "#f44d27") # taken from git's logo
-    set -l normal (set_color normal)
-    if not contains -- $PWD $GIT_REPOS_VISITED
-        set --append GIT_REPOS_VISITED $PWD
-        set -l git_repos_visited_count (count $GIT_REPOS_VISITED)
-        set -l repo_dir (string replace "$HOME" "~" $PWD)
-        _git_fish_echo (printf "added (%s%s%s) to list of visited repos, total: %s%d%s\n" \
-            $git_color $repo_dir $normal \
-            $git_color $git_repos_visited_count $normal)
+    set -l reset (set_color normal)
+    # query the database to see if the repo has been visited before
+    # if it has, then update the timestamp, and increment the number of times visited
+    # if it hasn't, then add it to the database
+    set -l unix_timestamp (date +%s)
+    set -l query "
+		INSERT INTO repos (path, timestamp) VALUES ('$PWD', $unix_timestamp)
+		ON CONFLICT(path) DO UPDATE SET timestamp=$unix_timestamp, number_of_times_visited=number_of_times_visited+1;
+	"
+    sqlite3 $GIT_FISH_REPOS_DB $query
+
+    # if the repo has not been visited before, then print a message
+    begin
+        set -l query "SELECT number_of_times_visited FROM repos WHERE path='$PWD';"
+        set -l number_of_times_visited (sqlite3 $GIT_FISH_REPOS_DB $query)
+        if test $number_of_times_visited -eq 1
+            set -l total_number_of_repos_visited (sqlite3 $GIT_FISH_REPOS_DB "SELECT COUNT(*) FROM repos;")
+            set -l repo_dir (string replace "$HOME" "~" $PWD)
+            _git_fish_echo (printf "added (%s%s%s) to list of visited repos, total: %s%d%s\n" \
+				$git_color $repo_dir $reset \
+				$git_color $total_number_of_repos_visited $reset)
+        end
     end
 
-    # add timestamp to the repo
-    # this will be used to sort the repos by the last time they were visited
-    set -l timestamp (date +%s)
-    sqlite3 ~/.config/git.fish/git.fish.db "INSERT INTO repos (path, timestamp) VALUES ('$PWD', $timestamp) ON CONFLICT(path) DO UPDATE SET timestamp=$timestamp;"
+    # if not contains -- $PWD $GIT_REPOS_VISITED
+    #     set --append GIT_REPOS_VISITED $PWD
+    #     set -l git_repos_visited_count (count $GIT_REPOS_VISITED)
+    #     set -l repo_dir (string replace "$HOME" "~" $PWD)
+    #     _git_fish_echo (printf "added (%s%s%s) to list of visited repos, total: %s%d%s\n" \
+    #         $git_color $repo_dir $reset \
+    #         $git_color $git_repos_visited_count $reset)
+    # end
+end
 
+function repos-list --description "list all the git repos that have been visited"
+    set -l git_color (set_color "#f44d27") # taken from git's logo
+    set -l reset (set_color normal)
+
+    set -l select_all_query "SELECT * FROM repos;"
+
+    if not isatty stdout
+        # for non-interactive shells, just print the list of repos
+        # so it is easier to pipe to other commands
+        sqlite3 $GIT_FISH_REPOS_DB $select_all_query | while read -l path timestamp number_of_times_visited
+            set -l repo_dir (string replace "$HOME" "~" $path)
+            printf "%s %d %d\n" $repo_dir $timestamp $number_of_times_visited
+        end
+    else
+        set -l paths
+        set -l timestamps
+        set -l number_of_times_visited_list
+        sqlite3 $GIT_FISH_REPOS_DB $select_all_query | while read -l path timestamp number_of_times_visited
+            set --append paths $path
+            set --append timestamps $timestamp
+            set --append number_of_times_visited_list $number_of_times_visited
+        end
+
+        set -l longest_path_length 0
+        for repo in $paths
+            set -l repo_length (string length $repo)
+            if test $repo_length -gt $longest_path_length
+                set longest_path_length $repo_length
+            end
+        end
+
+        # TODO: improve this output
+        # somehow incorporate the local status of the repo
+        # show it like in the neovim statusline with + and - and ~
+        # if the user is in a repo, the highlight it with a different color/or bold it
+        # do not hightlight the https://git{hub,lab}.com/ part of the url
+        # only the user/repo part
+        printf "%s%s%s\n" $git_color "repos visited:" $reset
+
+        set -l git_repos_visited_count (count $paths)
+
+        # 1 is added to the log10 of the count as the width will have to be
+        # at least 1 more than the log10 of the count e.g. 12 repos will have 2 digits
+        set -l index_width (math "floor(log10($git_repos_visited_count)) + 1")
+        set -l count 0
+        for repo in $paths
+            set count (math $count + 1)
+            set -l remote_origin_url (git -C $repo config --get remote.origin.url)
+            set -l repo_length (string length $repo)
+            set -l padding (string repeat --count (math $longest_path_length - $repo_length) ' ')
+            set -l directory_styling --bold
+            if test $repo = $PWD
+                set directory_styling blue --italics
+            end
+            set directory_styling (set_color $directory_styling)
+
+            #NOTE: <kpbaks 2023-05-21 22:29:44> Under system running SilverBlue the $HOME is /var/home/$USER
+            set repo (string replace --regex "^/var$HOME" "~" $repo | string replace --regex "^$HOME" "~")
+
+            printf "%"$index_width"d) " $count
+
+            printf "%s%s%s%s -> %s%s%s\n" $directory_styling $repo $reset $padding $git_color $remote_origin_url $reset
+        end
+
+    end
+end
+
+function repos-clear --description "clear the list of visited repos"
+    set -l query "DELETE FROM repos;"
+    sqlite3 $GIT_FISH_REPOS_DB $query
+    _git_fish_echo "cleared list of visited repos"
 end
 
 
@@ -55,59 +159,10 @@ function repos
         set -l verb $argv[1]
         switch $verb
             case clear
-                # The easiest way to clear the list of visited repos is to just delete the variable
-                # and then recreate it as an empty array.
-                set --erase GIT_REPOS_VISITED
-                set --universal GIT_REPOS_VISITED
-                echo "$prefix cleared list of visited repos"
+                repos-clear
                 return 0
             case list
-                if not isatty stdout
-                    # for non-interactive shells, just print the list of repos
-                    # so it is easier to pipe to other commands
-                    for repo in $GIT_REPOS_VISITED
-                        echo $repo
-                    end
-                else
-                    set -l longest_path_length 0
-                    for repo in $GIT_REPOS_VISITED
-                        set -l repo_length (string length $repo)
-                        if test $repo_length -gt $longest_path_length
-                            set longest_path_length $repo_length
-                        end
-                    end
-
-                    # TODO: improve this output
-                    # somehow incorporate the local status of the repo
-                    # show it like in the neovim statusline with + and - and ~
-                    # if the user is in a repo, the highlight it with a different color/or bold it
-                    # do not hightlight the https://git{hub,lab}.com/ part of the url
-                    # only the user/repo part
-                    printf "%s%s%s\n" $git_color "repos visited:" $normal
-                    set -l git_repos_visited_count (count $GIT_REPOS_VISITED)
-                    # 1 is added to the log10 of the count as the width will have to be
-                    # at least 1 more than the log10 of the count e.g. 12 repos will have 2 digits
-                    set -l index_width (math "floor(log10($git_repos_visited_count)) + 1")
-                    set -l count 0
-                    for repo in $GIT_REPOS_VISITED
-                        set count (math $count + 1)
-                        set -l remote_origin_url (git -C $repo config --get remote.origin.url)
-                        set -l repo_length (string length $repo)
-                        set -l padding (string repeat --count (math $longest_path_length - $repo_length) ' ')
-                        set -l directory_styling --bold
-                        if test $repo = $PWD
-                            set directory_styling blue --italics
-                        end
-                        set directory_styling (set_color $directory_styling)
-
-                        set repo (string replace --regex "^/var$HOME" "~" $repo | string replace --regex "^$HOME" "~")
-
-                        printf "%"$index_width"d) " $count
-
-                        printf "%s%s%s%s -> %s%s%s\n" $directory_styling $repo $normal $padding $git_color $remote_origin_url $normal
-                    end
-
-                end
+                repos-list
                 return 0
             case '*'
                 echo "$prefix unknown command: $verb"

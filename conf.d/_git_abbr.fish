259ef610e74583ffc61633f1d367997c3b4cdee4
kpbaks <kristoffer.pbs@tuta.io>
2023-05-24 22:48:42 +0200

    feat: add function to print the git abbreviations defined

--- conf.d/_git_abbr.fish
+++ conf.d/_git_abbr.fish
@@ -1,70 +1,150 @@
+# --------------------------------------------------------------------------------------------------
+# ideas:
+# - Add a way to list all the abbreviations specific to git.fish
+# --------------------------------------------------------------------------------------------------
 command --query git; or return
 command --query fzf; and set -g GIT_FISH_FZF_EXISTS
 
-abbr -a g "# You probably have an abbreviation for what you want to do ;)
+set -g GIT_FISH_ABBREVIAITONS
+set -g GIT_FISH_EXPANDED_ABBREVIAITONS
+
+function _git_abbr
+    set -l abbr $argv[1]
+    # set -l expanded (string collect $argv[2..])
+    set -l expanded $argv[2..]
+    abbr -a $argv
+    # set --append GIT_FISH_ABBREVIAITONS "$abbr :: $expanded"
+    set --append GIT_FISH_ABBREVIAITONS "$abbr"
+    set --append GIT_FISH_EXPANDED_ABBREVIAITONS "$expanded"
+    # set --append GIT_FISH_ABBREVIAITONS "$argv"
+end
+
+function git.fish.abbreviations
+    # printf " - %s\n" $GIT_FISH_ABBREVIAITONS
+    # echo $GIT_FISH_ABBREVIAITONS \
+    #     | sort \
+    #     | while read -l abbr
+    #     # echo "abbr: $abbr"
+    #     printf " - %s\n" $abbr
+    #     # set -l words (string split ' ' $abbr)
+    #     # set -l abbr $words[1]
+    #     # # set -l expanded "$words[2..]"
+    #     # printf " - %s\n" $abbr
+    #     # printf "   - %s\n" $expanded
+    #
+    # end
+
+    set -l length_of_longest_abbr 0
+    for abbr in $GIT_FISH_ABBREVIAITONS
+        set -l abbr_length (string length $abbr)
+        if test $abbr_length -gt $length_of_longest_abbr
+            set length_of_longest_abbr $abbr_length
+        end
+    end
+
+    #TODO: <kpbaks 2023-05-24 22:40:11> handle --regex abbrs
+
+    set -l abbreviation_heading abbreviation
+    set -l expanded_heading expanded
+    set -l padding_length (math $length_of_longest_abbr - (string length $abbreviation_heading))
+    set -l padding (string repeat --count $padding_length ' ')
+
+
+    set -l git_color (set_color "#f44d27") # taken from git's logo
+    set -l reset (set_color normal)
+    # printf "there are %s%d%s abbreviations\n" $git_color (count $GIT_FISH_ABBREVIAITONS) $reset
+    _git_fish_echo (printf "there are %s%d%s abbreviations\n" $git_color (count $GIT_FISH_ABBREVIAITONS) $reset)
+
+    set -l hr (string repeat --count $COLUMNS -)
+    echo $hr
+    printf "%s%s | %s\n" $abbreviation_heading $padding $expanded_heading
+    echo $hr
+
+
+    for i in (seq (count $GIT_FISH_ABBREVIAITONS))
+        set -l abbr $GIT_FISH_ABBREVIAITONS[$i]
+        set -l expanded $GIT_FISH_EXPANDED_ABBREVIAITONS[$i]
+        set -l abbr_length (string length $abbr)
+        set -l padding_length (math $length_of_longest_abbr - $abbr_length)
+        set -l padding (string repeat --count $padding_length ' ')
+        printf "%s%s | " $abbr $padding
+        echo "$expanded" | fish_indent --ansi
+        # set -l padding (string repeat ' ' (math (math $length_of_longest_abbr - (string length $abbr)) + 2))
+        # echo "$abbr + $expanded"
+
+    end
+    echo $hr
+
+    # for abbr in $GIT_FISH_ABBREVIAITONS
+    #     echo $abbr | fish_indent --ansi
+    # end
+    #
+end
+
+_git_abbr g "# You probably have an abbreviation for what you want to do ;)
 git"
 
 # git add
-abbr -a ga --set-cursor 'git add % && git status'
-abbr -a gaa 'git add --all && git status'
-abbr -a gam 'git ls-files --modified | xargs git add && git status'
-abbr -a gau 'git ls-files --others | xargs git add && git status'
-abbr -a gap 'git add --patch && git status'
+_git_abbr ga --set-cursor 'git add % && git status'
+_git_abbr gaa 'git add --all && git status'
+_git_abbr gam 'git ls-files --modified | xargs git add && git status'
+_git_abbr gau 'git ls-files --others | xargs git add && git status'
+_git_abbr gap 'git add --patch && git status'
 
 # git branch
 set -l git_branch_format "%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]"
 
-abbr -a gb git branch
-abbr -a gbl git branch --format="'$git_branch_format'" --sort=-committerdate
-abbr -a gba git branch --all --format="'$git_branch_format'" --sort=-committerdate
-abbr -a gbd git branch --delete
-abbr -a gbD git branch --delete --force
-abbr -a gbm git branch --move
+_git_abbr gb git branch
+_git_abbr gbl git branch --format="'$git_branch_format'" --sort=-committerdate
+_git_abbr gba git branch --all --format="'$git_branch_format'" --sort=-committerdate
+_git_abbr gbd git branch --delete
+_git_abbr gbD git branch --delete --force
+_git_abbr gbm git branch --move
 
 # git checkout
-abbr -a gco git checkout
+_git_abbr gco git checkout
 
 # git commit
-abbr -a gcm git commit
-abbr -a gcma git commit --amend
+_git_abbr gcm git commit
+_git_abbr gcma git commit --amend
 # conventional commits
 # https://daily-dev-tips.com/posts/git-basics-conventional-commits/
-abbr -a gcmb --set-cursor git commit --message "'build: %'"
-abbr -a gcmc --set-cursor git commit --message "'chore: %'"
-abbr -a gcmi --set-cursor git commit --message "'ci: %'"
-abbr -a gcmd --set-cursor git commit --message "'docs: %'"
-abbr -a gcmf --set-cursor git commit --message "'feat: %'"
-abbr -a gcmx --set-cursor git commit --message "'fix: %'"
-abbr -a gcmm --set-cursor git commit --message "'merge: %'"
-abbr -a gcmp --set-cursor git commit --message "'perf: %'"
-abbr -a gcmr --set-cursor git commit --message "'refactor: %'"
-abbr -a gcmv --set-cursor git commit --message "'revert: %'"
-abbr -a gcms --set-cursor git commit --message "'style: %'"
-abbr -a gcmt --set-cursor git commit --message "'test: %'"
+_git_abbr gcmb --set-cursor git commit --message "'build: %'"
+_git_abbr gcmc --set-cursor git commit --message "'chore: %'"
+_git_abbr gcmi --set-cursor git commit --message "'ci: %'"
+_git_abbr gcmd --set-cursor git commit --message "'docs: %'"
+_git_abbr gcmf --set-cursor git commit --message "'feat: %'"
+_git_abbr gcmx --set-cursor git commit --message "'fix: %'"
+_git_abbr gcmm --set-cursor git commit --message "'merge: %'"
+_git_abbr gcmp --set-cursor git commit --message "'perf: %'"
+_git_abbr gcmr --set-cursor git commit --message "'refactor: %'"
+_git_abbr gcmv --set-cursor git commit --message "'revert: %'"
+_git_abbr gcms --set-cursor git commit --message "'style: %'"
+_git_abbr gcmt --set-cursor git commit --message "'test: %'"
 
-abbr -a gcmB --set-cursor git commit --message "'build(%): '"
-abbr -a gcmC --set-cursor git commit --message "'chore(%): '"
-abbr -a gcmI --set-cursor git commit --message "'ci(%): '"
-abbr -a gcmD --set-cursor git commit --message "'docs(%): '"
-abbr -a gcmF --set-cursor git commit --message "'feat(%): '"
-abbr -a gcmX --set-cursor git commit --message "'fix(%): '"
-abbr -a gcmM --set-cursor git commit --message "'merge(%): '"
-abbr -a gcmP --set-cursor git commit --message "'perf(%): '"
-abbr -a gcmR --set-cursor git commit --message "'refactor(%): '"
-abbr -a gcmV --set-cursor git commit --message "'revert(%): '"
-abbr -a gcmS --set-cursor git commit --message "'style(%): '"
-abbr -a gcmT --set-cursor git commit --message "'test(%): '"
+_git_abbr gcmB --set-cursor git commit --message "'build(%): '"
+_git_abbr gcmC --set-cursor git commit --message "'chore(%): '"
+_git_abbr gcmI --set-cursor git commit --message "'ci(%): '"
+_git_abbr gcmD --set-cursor git commit --message "'docs(%): '"
+_git_abbr gcmF --set-cursor git commit --message "'feat(%): '"
+_git_abbr gcmX --set-cursor git commit --message "'fix(%): '"
+_git_abbr gcmM --set-cursor git commit --message "'merge(%): '"
+_git_abbr gcmP --set-cursor git commit --message "'perf(%): '"
+_git_abbr gcmR --set-cursor git commit --message "'refactor(%): '"
+_git_abbr gcmV --set-cursor git commit --message "'revert(%): '"
+_git_abbr gcmS --set-cursor git commit --message "'style(%): '"
+_git_abbr gcmT --set-cursor git commit --message "'test(%): '"
 
 # git config
-abbr -a gcfg git config
-abbr -a gcfgl git config --list
-abbr -a gcfgg git config --global
-abbr -a gcfgl git config --local
+_git_abbr gcfg git config
+_git_abbr gcfgl git config --list
+_git_abbr gcfgg git config --global
+_git_abbr gcfgl git config --local
 
 
 # git diff
-abbr -a gd git diff HEAD
-abbr -a gds git diff --stat HEAD
+_git_abbr gd git diff HEAD
+_git_abbr gds git diff --stat HEAD
 
 function parse_git_difftool_tool_help_output
     command git difftool --tool-help | sed --regexp-extended -e '/Use/! d' -e 's/\s*(\w+)\s+(Use.+)$/\1\t\2/'
@@ -84,64 +164,64 @@ function abbr_git_difftool
 end
 
 # git difftool
-abbr -a gdt --set-cursor --function abbr_git_difftool
+_git_abbr gdt --set-cursor --function abbr_git_difftool
 
 # git grep
-abbr -a gg git grep
+_git_abbr gg git grep
 
 # git log
-abbr -a gl git log --graph
-abbr -a glo git log --oneline --decorate --graph --all
+_git_abbr gl git log --graph
+_git_abbr glo git log --oneline --decorate --graph --all
 
 # git merge
-abbr -a gm git merge
-abbr -a gma git merge --abort
-abbr -a gmc git merge --continue
+_git_abbr gm git merge
+_git_abbr gma git merge --abort
+_git_abbr gmc git merge --continue
 
 
 # git mv
-abbr -a gmv git mv
+_git_abbr gmv git mv
 
 # git pull
-abbr -a gp git pull --progress
-abbr -a pull git pull --progress
+_git_abbr gp git pull --progress
+_git_abbr pull git pull --progress
 # git push
-abbr -a gP git push --progress
-abbr -a push git push --progress
+_git_abbr gP git push --progress
+_git_abbr push git push --progress
 
 # git rebase
-abbr -a grb git rebase
-abbr -a grbi git rebase --interactive
+_git_abbr grb git rebase
+_git_abbr grbi git rebase --interactive
 
 # git reflog
-abbr -a grl git reflog
+_git_abbr grl git reflog
 
 
 # git restore
-abbr -a gr git restore
+_git_abbr gr git restore
 
 # git rm
-abbr -a grm git rm
+_git_abbr grm git rm
 
 
 # git show
-abbr -a gsh git show
+_git_abbr gsh git show
 
 # git status
-abbr -a gs git status --untracked-files=all
-abbr -a gss git status --short --branch --untracked-files=all
+_git_abbr gs git status --untracked-files=all
+_git_abbr gss git status --short --branch --untracked-files=all
 
 # git stash
-abbr -a gst git stash
-abbr -a gstp git stash pop
-abbr -a gsta git stash apply
-abbr -a gstd git stash drop
-abbr -a gstl git stash list
+_git_abbr gst git stash
+_git_abbr gstp git stash pop
+_git_abbr gsta git stash apply
+_git_abbr gstd git stash drop
+_git_abbr gstl git stash list
 
 # git submodule
-abbr -a gsm git submodule
-abbr -a gsms git submodule status
-abbr -a gsml git submodule status
+_git_abbr gsm git submodule
+_git_abbr gsms git submodule status
+_git_abbr gsml git submodule status
 
 
 # git switch
@@ -176,23 +256,23 @@ function abbr_git_switch
     echo -- "$cmd"
 end
 
-abbr -a gsw --function abbr_git_switch
+_git_abbr gsw --function abbr_git_switch
 
 
 # git worktree
-abbr -a gwt git worktree
+_git_abbr gwt git worktree
 # it is best practive to create a worktree in a directory that is a sibling of the current directory
 function abbr_git_worktree_add
     set -l dirname (path basename $PWD)
     set -l worktree_dirname "$dirname-wt"
     echo -- git worktree add "../$worktree_dirname/%" --detach
 end
-abbr -a gwta --set-cursor --function abbr_git_worktree_add
-abbr -a gwtl git worktree list
-abbr -a gwtm git worktree move
-abbr -a gwtp git worktree prune
-abbr -a gwtrm git worktree remove
-abbr -a gwtrmf git worktree remove --force
+_git_abbr gwta --set-cursor --function abbr_git_worktree_add
+_git_abbr gwtl git worktree list
+_git_abbr gwtm git worktree move
+_git_abbr gwtp git worktree prune
+_git_abbr gwtrm git worktree remove
+_git_abbr gwtrmf git worktree remove --force
 
 #
 
@@ -217,12 +297,12 @@ function abbr_git_clone
     echo -- git clone $args $postfix_args
 end
 
-abbr -a git_clone_at_depth --position command --regex "gc[0-9]*" --function abbr_git_clone
+_git_abbr git_clone_at_depth --position command --regex "gc[0-9]*" --function abbr_git_clone
 
 set -l sleep_duration 1.5
 
-abbr -a gac --set-cursor "git add --update && git status && sleep $sleep_duration && git commit"
-abbr -a gacp --set-cursor "git add --update % && git status && sleep $sleep_duration && git commit && git push"
+_git_abbr gac --set-cursor "git add --update && git status && sleep $sleep_duration && git commit"
+_git_abbr gacp --set-cursor "git add --update % && git status && sleep $sleep_duration && git commit && git push"
 
 set --erase GIT_FISH_FZF_EXISTS
 
@@ -230,7 +310,7 @@ set --erase GIT_FISH_FZF_EXISTS
 # other git tools -------------------------------------------------------------
 
 # lazygit
-abbr -a lg lazygit
+_git_abbr lg lazygit
 
 # gitui
-abbr -a gui gitui
+_git_abbr gui gitui

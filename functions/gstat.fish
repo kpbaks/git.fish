1a737ee35bb828d38947426aceb0577dcec86b4a
kpbaks <kristoffer.pbs@tuta.io>
2023-09-08 20:31:38 +0200

    feat(gstat): use either `find` or `fd`

--- functions/gstat.fish
+++ functions/gstat.fish
@@ -1,22 +1,24 @@
 function gstat
-    if not command --query find
-        set_color red --bold
-        echo "find not found"
-        set_color normal
-        return 1
-    end
 
-    set -l options (fish_opt --short=l --long=no-log --long-only)
-    set --append options (fish_opt --short=t --long=no-stash --long-only)
-    set --append options (fish_opt --short=s --long=no-status --long-only)
-    set --append options (fish_opt --short=b --long=no-branch --long-only)
-    set --append options (fish_opt --short=h --long=help)
+    set --local options (fish_opt --short=l --long=no-log --long-only)
+    set --append options (fish_opt --short=n --long=no --longonly --required-val --multiple-vals)
+    # TODO: <kpbaks 2023-09-08 17:08:14> --no branch,stash,status,log etc.
+    # set --append options (fish_opt --short=t --long=no-stash --long-only)
+    # set --append options (fish_opt --short=s --long=no-status --long-only)
+    # set --append options (fish_opt --short=b --long=no-branch --long-only)
+    # set --append options (fish_opt --short=h --long=help)
 
     if not argparse $options -- $argv
         return 1
     end
 
-    set -l git_color (set_color "#f44d27") # taken from git's logo
+    set --local reset (set_color normal)
+	set --local bold (set_color --bold)
+	set --local green (set_color green)
+	set --local yellow (set_color yellow)
+	set --local red (set_color red)
+    set --local blue (set_color blue)
+    set --local git_color (set_color "#f44d27") # taken from git's logo
 
     if set --query _flag_help
         set_color green --bold
@@ -32,13 +34,26 @@ function gstat
         return 0
     end
 
-    set -l normal (set_color normal)
-    set -l bold (set_color --bold)
-    set -l dir_fmt (set_color green)
-    set -l repo_fmt (set_color yellow)
+    set --local find_cmd
+    set --local find_cmd_opts
+    if command --query fd
+		set find_cmd fd
+		set find_cmd_opts --hidden --type d --max-depth $maxdepth --no-ignore
+	else if command --query find
+    	set find_cmd find
+		set find_cmd_opts -L
+	else
+        printf "%serror:%s Neither `find` nor `fd` are available\n" $red $reset
+        return 1
+    end
 
-    set -l maxdepth 3
-    set -l border_count (math "min($COLUMNS, 100)")
+    set --local normal (set_color normal)
+    set --local bold (set_color --bold)
+    set --local dir_fmt (set_color green)
+    set --local repo_fmt (set_color yellow)
+
+    set --local maxdepth 3
+    set --local border_count (math "min($COLUMNS, 100)")
 
     # TODO: submodules
     # mimic display-modes of https://github.com/nickgerace/gfold
@@ -46,7 +61,7 @@ function gstat
     # - classic
     # - standard (It could be called --gfold)
 
-    set -l root_dir $PWD
+    set --local root_dir $PWD
     if test (count $argv) -gt 0
         set root_dir $argv[1]
     end
@@ -59,12 +74,12 @@ function gstat
     end
 
 
-    set -l found_count 0
+    set --local found_count 0
     for git_dir in (find $root_dir -maxdepth $maxdepth -type d -name ".git")
         set found_count (math "$found_count + 1")
         set repo_dir (path dirname $git_dir)
-        set -l git git -C $repo_dir
-        set -l remote_url ($git config --local --get remote.origin.url)
+        set --local git git -C $repo_dir
+        set --local remote_url ($git config --local --get remote.origin.url)
 
         printf "%sdir%s:  %s%s%s\n" \
             $bold $normal \
@@ -73,17 +88,17 @@ function gstat
             $bold $normal \
             $repo_fmt $remote_url $normal
         if not set --query _flag_no_branch
-            set -l active_branch ($git rev-parse --abbrev-ref HEAD)
-            set -l inactive_branches ($git branch | string match --regex --invert "\* (\S+)" | string trim --chars=" " --)
+            set --local active_branch ($git rev-parse --abbrev-ref HEAD)
+            set --local inactive_branches ($git branch | string match --regex --invert "\* (\S+)" | string trim --chars=" " --)
 
-            set -l active_branch_fmt $git_color
+            set --local active_branch_fmt $git_color
             if test -z $inactive_branches
                 printf "%sbranch%s: { %s%s%s }\n" \
                     $bold $normal \
                     $active_branch_fmt $active_branch $normal
             else
-                set -l grey "#888888"
-                set -l inactive_branch_fmt (set_color $grey)
+                set --local grey "#888888"
+                set --local inactive_branch_fmt (set_color $grey)
                 printf "%sbranches%s: { %s%s%s" \
                     $bold $normal \
                     $active_branch_fmt $active_branch $normal
@@ -100,7 +115,7 @@ function gstat
         end
 
         if not set --query _flag_no_stash
-            set -l stashes ($git stash list)
+            set --local stashes ($git stash list)
             if test -n $stashes
                 printf "%s%s%s:\n" \
                     (set_color --bold) \
@@ -125,7 +140,7 @@ function gstat
         echo "no git repos found"
         set_color normal
     else
-        set -l green (set_color green --bold)
+        set --local green (set_color green --bold)
         printf "found %s%d%s git %s in the subtree of %s%s%s\n" \
             $green $found_count $normal \
             (test $found_count -eq 1; and echo "repo"; or echo "repos") \

e793deb08326006cb66738b802cc4894ac471b25
kpbaks <kristoffer.pbs@tuta.io>
2023-09-13 13:17:47 +0200

    feat(completions): finish completions for `gi`

--- completions/gi.fish
+++ completions/gi.fish
@@ -1,5 +1,6 @@
 set -l C complete --command gi
 
 $C -s h -l help -d 'Show help information'
+$C -s m -l merge -d 'Merge with existing .gitignore file'
 
 $C -a '(gi list | string replace --regex --all "[ ,]" "\n")'

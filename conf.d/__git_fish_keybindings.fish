5b4cc062c2519ca91fa5320c159befc89a7c5738
kpbaks <kristoffer.pbs@tuta.io>
2023-09-13 13:21:44 +0200

    feat: make keybindings functionality opt-in rather than mandatory

--- conf.d/__git_fish_keybindings.fish
+++ conf.d/__git_fish_keybindings.fish
@@ -1,5 +1,9 @@
 status is-interactive; or return
 
+# Make this feature opt-in
+set --query GIT_FISH_GITUI_KEYBIND_ENABLE; or set --universal GIT_FISH_GITUI_KEYBIND_ENABLE 0
+test "$GIT_FISH_GITUI_KEYBIND_ENABLE" = 1; or return
+
 if command --query gitui,
     __git.fish::echo "gitui is installed, binding ctrl+g to open gitui"
     bind \cg 'gitui; commandline -f repaint'
@@ -10,5 +14,5 @@ else if command --query tig
     __git.fish::echo "tig is installed, binding ctrl+g to open tig"
     bind \cg 'tig; commandline -f repaint'
 else
-    # __git.fish::echo "no git ui is installed, binding ctrl+g to open gitk"
+    __git.fish::echo "no git ui is installed, binding ctrl+g to open gitk"
 end

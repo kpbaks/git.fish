33df5a2ccdef8e0a0b3aaf299526e0b329b6588c
kpbaks <kristoffer.pbs@tuta.io>
2023-09-15 20:55:29 +0200

    feat(gcl): config items which are executable commands are now highlighted with `fish_indent --ansi`

--- functions/gcl.fish
+++ functions/gcl.fish
@@ -1,13 +1,12 @@
 function gcl --description "Print the git config in a nice format"
-    set -l options (fish_opt --short h --long help)
-    # specifying the short flag -l is necessary even though it is not valid. (why fish?)
-    set -a options (fish_opt --short l --long local --long-only)
+    set --local options (fish_opt --short h --long help)
+    set --append options (fish_opt --short l --long local)
     if not argparse $options -- $argv
         return 1
     end
 
     if set --query _flag_help
-        set -l usage "$(set_color --bold)Print the git config in a nice format$(set_color normal)
+        set --local usage "$(set_color --bold)Print the git config in a nice format$(set_color normal)
 
 $(set_color yellow)Usage:$(set_color normal) $(set_color blue)$(status current-command)$(set_color normal) [options]
 
@@ -22,13 +21,13 @@ Part of $(set_color cyan)git.fish$(set_color normal) at https://github.com/kpbs5
     end
 
 
-    set -l default_param_color (set_color blue)
-    set -l important_param_color (set_color yellow)
-    set -l normal (set_color normal)
-    set -l bar "│"
-    set -l output_separator " $bar "
-    set -l params
-    set -l values
+    set --local default_param_color (set_color blue)
+    set --local important_param_color (set_color yellow)
+    set --local reset (set_color normal)
+    set --local bar "│"
+    set --local output_separator " $bar "
+    set --local params
+    set --local values
     # Read all the params and values into two arrays
     command git config $_flag_local --list \
         | sort \
@@ -38,19 +37,21 @@ Part of $(set_color cyan)git.fish$(set_color normal) at https://github.com/kpbs5
     end
 
     # Find the longest param and value
-    set -l character_length_of_longest_param 0
+    set --local character_length_of_longest_param 0
     for param in $params
-        set -l length (string length $param)
-        if test $length -gt $character_length_of_longest_param
-            set character_length_of_longest_param $length
-        end
+        set --local length (string length $param)
+        set character_length_of_longest_param (math "max $character_length_of_longest_param,$length")
+        # if test $length -gt $character_length_of_longest_param
+        #     set character_length_of_longest_param $length
+        # end
     end
-    set -l character_length_of_longest_value 0
+    set --local character_length_of_longest_value 0
     for value in $values
-        set -l length (string length $value)
-        if test $length -gt $character_length_of_longest_value
-            set character_length_of_longest_value $length
-        end
+        set --local length (string length $value)
+        set character_length_of_longest_value (math "max $character_length_of_longest_value,$length")
+        # if test $length -gt $character_length_of_longest_value
+        #     set character_length_of_longest_value $length
+        # end
     end
 
     if set --query _flag_local
@@ -60,29 +61,32 @@ Part of $(set_color cyan)git.fish$(set_color normal) at https://github.com/kpbs5
     end
 
     for i in (seq (count $params))
-        set -l param (string trim $params[$i])
-        set -l value (string trim $values[$i])
+        set --local param (string trim $params[$i])
+        set --local value (string trim $values[$i])
         # Skip params that are empty or comments
-        if string match --quiet --regex "^\s*\$" -- $value
-            continue
-        end
-        set -l length_of_param (string length $param)
-        set -l length_of_value (string length $value)
-        set -l padding_length_of_param (math "$character_length_of_longest_param - $length_of_param")
-        set -l padding_length_of_value (math "$character_length_of_longest_value - $length_of_value")
-        set -l padding_of_param (string repeat --count $padding_length_of_param " ")
-        set -l padding_of_value (string repeat --count $padding_length_of_value " ")
+        string match --quiet --regex "^\s*\$" -- $value; and continue
 
-        set -l value_color $normal
+        set --local length_of_param (string length $param)
+        set --local length_of_value (string length $value)
+        set --local padding_length_of_param (math "$character_length_of_longest_param - $length_of_param")
+        set --local padding_length_of_value (math "$character_length_of_longest_value - $length_of_value")
+        set --local padding_of_param (string repeat --count $padding_length_of_param " ")
+        set --local padding_of_value (string repeat --count $padding_length_of_value " ")
+
+        set --local value_color $reset
         if test $value = true
             set value_color (set_color --bold green)
         else if test $value = false
             set value_color (set_color --bold red)
         else if string match --quiet --regex "^\d+\$" -- $value
             set value_color (set_color --bold purple)
+        else if string match --quiet --regex "^!" -- $value
+            set value_color $reset
+            # Wrap in `printf` to remove trailing newline
+            set value (printf (string sub --start 2 -- $value | fish_indent --ansi))
         end
 
-        set -l param_color $default_param_color
+        set --local param_color $default_param_color
         if test $param = "user.name"
             set param_color $important_param_color
             set value_color $important_param_color
@@ -97,25 +101,25 @@ Part of $(set_color cyan)git.fish$(set_color normal) at https://github.com/kpbs5
             # TODO: get this to work
             # values starting with ! are shell commands
             # if string match --quiet --regex "^!" -- $value
-            #     set value (echo $value | fish_indent --ansi)
-            # end
+            #     set value huh
+            # set value (printf (echo $value | fish_indent --ansi))
         end
 
-        set -l param_cell (printf "%s%s%s%s" $param_color $param $normal $padding_of_param)
-        set -l separator_cell (printf "%s" $output_separator)
-        set -l row_length (math "$(string length $param_cell) + $(string length $separator_cell) + $(string length $value)")
-        set -l ellipsis " ... "
-        set -l ellipsis_length (string length $ellipsis)
-        set -l free_space (math "$COLUMNS - $ellipsis_length")
+        set --local param_cell (printf "%s%s%s%s" $param_color $param $reset $padding_of_param)
+        set --local separator_cell (printf "%s" $output_separator)
+        set --local row_length (math "$(string length $param_cell) + $(string length $separator_cell) + $(string length $value)")
+        set --local ellipsis " ... "
+        set --local ellipsis_length (string length $ellipsis)
+        set --local free_space (math "$COLUMNS - $ellipsis_length")
 
         if test $row_length -gt $free_space
             # Ellipsize the value cell
             # TODO: not entirely accurate, but good enough for now
-            set -l cutoff (math "$free_space - $(string length $param_cell) - $(string length $separator_cell)")
+            set --local cutoff (math "$free_space - $(string length $param_cell) - $(string length $separator_cell)")
             set value (string sub --start 1 --end $cutoff -- $value)
-            set value (printf "%s%s%s%s" $value (set_color --bold red) $ellipsis $normal)
+            set value (printf "%s%s%s%s" $value (set_color --bold red) $ellipsis $reset)
         end
-        set -l value_cell (printf "%s%s%s" $value_color $value $normal)
+        set --local value_cell (printf "%s%s%s" $value_color $value $reset)
 
         printf "%s%s%s\n" \
             $param_cell \

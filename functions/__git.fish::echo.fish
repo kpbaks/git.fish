dd53da96ab75f898835beffbf8dcb569ea201670
kpbaks <kristoffer.pbs@tuta.io>
2023-09-15 20:42:08 +0200

    fix: __git.fish::echo outputs to stderr instead of stdout

--- functions/__git.fish::echo.fish
+++ functions/__git.fish::echo.fish
@@ -1,6 +1,6 @@
 function __git.fish::echo
-    set -l git_color (set_color "#f44d27") # taken from git's logo
-    set -l normal (set_color normal)
-    set -l prefix (printf "%s[git.fish]%s" $git_color $normal)
-    echo "$prefix $argv"
+    set --local git_color (set_color "#f44d27") # taken from git's logo
+    set --local reset (set_color normal)
+    set --local prefix (printf "%s[git.fish]%s" $git_color $reset)
+    echo "$prefix $argv" >&2
 end

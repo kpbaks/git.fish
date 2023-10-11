b559bd793a0e7febc96e796c1e9080624a60a8bc
kpbaks <kristoffer.pbs@tuta.io>
2023-10-11 17:36:31 +0200

    feat(pre-commit): change settings variable to always be defined and instead use 0/1 to denote disabled/enabled

--- conf.d/__git_fish_pre_commit.fish
+++ conf.d/__git_fish_pre_commit.fish
@@ -16,10 +16,14 @@ if not command --query pre-commit
 end
 
 # Disable by default
-set --query GIT_FISH_CHECK_FOR_PRE_COMMIT_ENABLED; or set --universal GIT_FISH_CHECK_FOR_PRE_COMMIT_ENABLED 0
+set --query GIT_FISH_PRE_COMMIT_ENABLE; or set --universal GIT_FISH_PRE_COMMIT_ENABLE 0
+set --query GIT_FISH_PRE_COMMIT_LIST_HOOKS; or set --universal GIT_FISH_PRE_COMMIT_LIST_HOOKS 0
+set --query GIT_FISH_PRE_COMMIT_AUTO_INSTALL; or set --universal GIT_FISH_PRE_COMMIT_AUTO_INSTALL 0
 
 function __git.fish::check_for_pre_commit --on-event in_git_repo_root_directory
-    test $GIT_FISH_CHECK_FOR_PRE_COMMIT_ENABLED -eq 1; or return
+    test $GIT_FISH_PRE_COMMIT_ENABLE -eq 1; or return
+    # TODO: <kpbaks 2023-10-11 17:23:59> have a global variable that all git.fish functions can use
+    # for similar purposes
     set --query __git_fish_git_directories_visisted
     or set --global __git_fish_git_directories_visisted
     # Do not want to spam the user with the same message over and over again in the same shell session
@@ -32,39 +36,32 @@ function __git.fish::check_for_pre_commit --on-event in_git_repo_root_directory
 
     if test -f .pre-commit-config.yaml
         __git.fish::echo "A $dot_pre_commit_config_yaml file was found in $(set_color --bold)$cwd$(set_color normal)"
-        if set --query GIT_FISH_PRE_COMMIT_LIST_HOOKS
+        if test $GIT_FISH_PRE_COMMIT_LIST_HOOKS -eq 1
             set --local hooks (string match --regex --all --groups-only "[^#]+-\s+id:\s(\S+)\$" < .pre-commit-config.yaml)
-            __git.fish::echo "the following hooks are listed:"
+            __git.fish::echo "The following hooks are listed:"
             printf " - %s\n" $hooks
         end
         if not test -f ./.git/hooks/pre-commit
             __git.fish::echo "pre-commit hooks not installed. installing..."
             command pre-commit install --install-hooks 2>/dev/null
-            # __git.fish::echo "pre-commit hooks installed."
         else
             __git.fish::echo "pre-commit hooks are installed $(set_color green)✓$(set_color normal)"
-            # set --local auto_update_command (echo -n "pre-commit autoupdate" | fish_indent --ansi)
-            # __git.fish::echo "to autoupdate them, run: $auto_update_command"
         end
     else
-        __git.fish::echo "no $dot_pre_commit_config_yaml file found in $(set_color --bold)$cwd$(set_color normal)."
-        if set --query GIT_FISH_PRE_COMMIT_AUTO_INSTALL
-            __git.fish::echo "GIT_FISH_PRE_COMMIT_AUTO_INSTALL is set. generating a sample $dot_pre_commit_config_yaml file and installing it..."
+        __git.fish::echo "No $dot_pre_commit_config_yaml file found in $(set_color --bold)$cwd$(set_color normal)."
+        if test $GIT_FISH_PRE_COMMIT_AUTO_INSTALL -eq 1
+            __git.fish::echo "\$GIT_FISH_PRE_COMMIT_AUTO_INSTALL is set. generating a sample $dot_pre_commit_config_yaml file and installing it..."
             command pre-commit sample-config | tee .pre-commit-config.yaml && command pre-commit install --install-hooks
             __git.fish::echo "pre-commit hooks installed."
         else
             set --local generate_sample_config_command "pre-commit sample-config | tee .pre-commit-config.yaml && pre-commit install"
             set --local abbreviation pcg
-            __git.fish::echo (printf "a sample $dot_pre_commit_config_yaml file can be generated and installed with: (use the abbreviation %s$abbreviation%s to run it)" (set_color $fish_color_command) (set_color normal))
+            __git.fish::echo (printf "A sample $dot_pre_commit_config_yaml file can be generated and installed with: (use the abbreviation %s$abbreviation%s to run it)" (set_color $fish_color_command) (set_color normal))
             echo -en "\t"
             echo "$generate_sample_config_command" | fish_indent --ansi
             if not abbr --query $abbreviation
-                # __git.fish::echo "adding abbreviation: $(set_color --reverse)$abbreviation$(set_color normal) for"
-                # echo -en "\t"
-                # echo "$generate_sample_config_command" | fish_indent --ansi
                 abbr --add $abbreviation "$generate_sample_config_command"
             end
-            # __git.fish::echo "the abbreviation $(set_color --reverse)$abbreviation$(set_color normal) can be used to generate a sample $dot_pre_commit_config_yaml file."
         end
     end
 

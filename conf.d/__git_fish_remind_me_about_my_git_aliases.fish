c246a63b16341a814d4cf5fb9d15f1bd7dd04c8c
kpbaks <kristoffer.pbs@tuta.io>
2024-01-16 16:30:57 +0100

    fix: detect when subcommand with an git alias is used properly

--- conf.d/__git_fish_remind_me_about_my_git_aliases.fish
+++ conf.d/__git_fish_remind_me_about_my_git_aliases.fish
@@ -1,16 +1,14 @@
 status is-interactive; or return 0
 
-set --query GIT_FISH_GIT_ALIAS_REMINDER_ENABLE; or set --universal GIT_FISH_GIT_ALIAS_REMINDER_ENABLE 0
-test $GIT_FISH_GIT_ALIAS_REMINDER_ENABLE -eq 1; or return 0
+set --query GIT_FISH_REMIND_ME_ABOUT_MY_GIT_ALIASES; or set --global GIT_FISH_REMIND_ME_ABOUT_MY_GIT_ALIASES 0
+test $GIT_FISH_REMIND_ME_ABOUT_MY_GIT_ALIASES -eq 1; or return 0
 
 function __git.fish::remind_me_about_my_git_aliases --on-event fish_postexec
     # if the user typed a git command, remind them about their aliases
     # but only if they used a subcommand that they have an alias for.
-    set --local cmd $argv[1]
-    test $cmd = git; or return 0
-
-    set --local subcmd $argv[2]
-    test -n $subcmd; or return 0 # no subcommand
+    set argv (string split " " -- $argv)
+    test $argv[1] = git; or return 0
+    test (count $argv) -gt 1; or return 0 # no subcommand
 
     command git config --list \
         | string match --regex "^alias.*" \
@@ -20,9 +18,12 @@ function __git.fish::remind_me_about_my_git_aliases --on-event fish_postexec
         test "$expansion" = "$argv[2..]"; or continue
         # if the user typed the expansion of an alias remind the person
         # that the alias exists
-        __git.fish::echo "STOP WHAT YOU'RE DOING! You have a git alias for this command:"
-        printf "%s%s%s -> " (set_color $fish_color_param) $alias (set_color normal)
-
-        echo $expansion | fish_indent --ansi
+        set -l reset (set_color normal)
+        __git.fish::echo "STOP WHAT YOU'RE DOING! You have a $(set_color red)git alias$(set_color normal) for this subcommand:"
+        printf "\t"
+        printf "%s%s" (echo "git $alias" | fish_indent --ansi) $reset
+        printf " -> "
+        printf "%s%s" (echo "git $expansion" | fish_indent --ansi) $reset
+        printf "\n"
     end
 end

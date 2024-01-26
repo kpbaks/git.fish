1feab3d38a744168714d8192da168b825590d3b3
kpbaks <kristoffer.pbs@tuta.io>
2024-01-26 14:30:34 +0100

    feat(ga): improve visuals of fzf prompt

--- functions/ga.fish
+++ functions/ga.fish
@@ -21,20 +21,50 @@ function ga
     # TODO: what if a file has contents that are both staged and modified?
 
     set -l fzf_preview "git diff --color=always {2}"
+    set -l git_color (set_color "#f44d27") # taken from git's logo
 
     # TODO: improve colors
     set -l fzf_opts \
         --ansi \
         --multi \
+        --border=none \
         --height=80% \
         --reverse \
+        --cycle \
+        --pointer='|>' \
+        --marker='✓ ' \
+        --no-mouse \
+        --prompt=$prompt \
+        --exit-0 \
+        --header-first \
+        --scroll-off=5 \
+        --color='marker:#00ff00' \
+        --color="header:#$fish_color_command" \
+        --color="info:#$fish_color_keyword" \
+        --color="prompt:#$fish_color_autosuggestion" \
+        --color='border:#f44d27' \
+        --color="gutter:-1" \
+        --color="hl:#FFB600" \
+        --color="hl+:#FFB600" \
+        --no-scrollbar \
+        --bind=ctrl-a:select-all \
+        --bind=ctrl-z:deselect-all \
+        --bind=ctrl-d:preview-page-down \
+        --bind=ctrl-u:preview-page-up \
+        --bind=ctrl-f:page-down \
+        --bind=ctrl-b:page-up \
+        --border-label=" $(string upper "git.fish") " \
+        --header="Select which modified files to stage. <ctrl-a> to select all. <ctrl-z> to deselect all" \
         --preview="$fzf_preview"
+        # --header-lines=3 \
+
     set -l reset (set_color normal)
     set -l blue (set_color blue)
 
     set -l selected_files (
         command git -c color.status=always status --short \
         | while read modification_status file_path
+            # TODO: align both columns properly
             set -l dirname (path dirname $file_path)
             if test $dirname = "."
                 printf "%s\t%s\n" $modification_status $file_path
